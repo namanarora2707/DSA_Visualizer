cdfdd7d4fd9799c50ae2863176e0a5315b758c6f
namanarora2707 <namanarora270704@gmail.com>
2025-11-12 21:24:24 +0530

    done

--- client/components/ControlPanel.jsx
+++ client/components/ControlPanel.jsx
@@ -71,6 +71,31 @@ export default function ControlPanel({ type, onOperate }){
             </div>
           </div>
         );
+      case "doubly-linked-list":
+        return (
+          <div className="stack">
+            <input value={value} onChange={(e)=>setValue(e.target.value)} placeholder="Value" className="input" />
+            <div className="grid2">
+              <button className="btn" onClick={()=>onOperate({ op:"insertHead", args:value })}>Insert Head</button>
+              <button className="btn" onClick={()=>onOperate({ op:"insertTail", args:value })}>Insert Tail</button>
+              <button className="btnabcd" onClick={()=>onOperate({ op:"delete", args:value })}>Delete</button>
+              <br />
+              <button className="btnabcd" onClick={()=>onOperate({ op:"insertAt", args:value })}>Insert At (index--value)</button>
+              <button className="btnabcd" onClick={()=>onOperate({ op:"deleteAt", args:value })}>Delete At (index)</button>
+              <button className="btnabcd" onClick={()=>onOperate({ op:"update", args:value })}>Update (index--newvalue)</button>
+            </div>
+          </div>
+        );
+      case "binary-tree":
+        return (
+          <div className="stack">
+            <input type="number" value={value} onChange={(e)=>setValue(e.target.value)} placeholder="Number" className="input" />
+            <div className="grid2">
+              <button className="btn" onClick={()=>onOperate({ op:"insert", args:Number(value) })}>Insert</button>
+              <button className="btn" onClick={()=>onOperate({ op:"delete", args:Number(value) })}>Delete</button>
+            </div>
+          </div>
+        );
       default:
         return null;
     }
